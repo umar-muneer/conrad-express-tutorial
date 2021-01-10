318453b7fa5f0e4d1087062810bab0fcf4802be4
Umar Muneer <umarmuneer@gmail.com>
2021-01-10 17:22:24 +0500

    add the first route

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/health', (req, res) => res.json("OK"));
+
 app.listen(port, () => console.log(`listening on port ${port}`));
