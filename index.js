310a9257e53dabaf80a58e226fe69b291b6835c9
Umar Muneer <umarmuneer@gmail.com>
2021-01-11 12:17:39 +0500

    move courses api to its own router

--- index.js
+++ index.js
@@ -1,69 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bunyanLogger = require("express-bunyan-logger");
-const collection = [];
+const {courses} = require('./routers');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const checkCourseExists = (req, res, next) => {
-  const courseId = parseInt(req.params.course_id);
-  const courseIndex = collection.findIndex((course) => course.id === courseId);
-  if (courseIndex === -1) {
-    res.status(404).json({
-      message: "requested resource was not found",
-    });
-    return;
-  }
-  req.courseIndex = courseIndex;
-  next();
-};
-const validateCoursePostRequest = (req, res, next) => {
-  const { id, name } = req.body;
-  if (isNaN(parseInt(id))) {
-    res.status(400).json({
-      message: "the id of the course should be a number",
-    });
-    return;
-  }
-  if (!name) {
-    res.status(400).json({
-      message: "the name of the course is required",
-    });
-    return;
-  }
-  next();
-};
 app.use(bodyParser.json());
 app.use(bunyanLogger());
 app.get("/health", (req, res) => res.json("OK"));
-app.post("/courses", validateCoursePostRequest, (req, res) => {
-  const { id, name } = req.body;
-  collection.push({ id, name });
-  res.json({
-    id,
-    name,
-  }); // sending a json request with the collection array as the body and status 200
-});
-app.get("/courses", (req, res) => {
-  const randomHeader = req.get('x-random-header');
-  res.set('x-random-header', randomHeader);
-  res.json(collection);
-});
-app.put("/courses/:course_id", checkCourseExists, (req, res) => {
-  const { name } = req.body;
-  const courseId = parseInt(req.params.course_id);
-  const updatedCourse = {
-    id: courseId,
-    name,
-  };
-  collection[req.courseIndex] = updatedCourse;
-  res.status(200).json(collection[req.courseIndex]);
-});
-app.delete("/courses/:course_id", checkCourseExists, (req, res) => {
-  const courseId = parseInt(req.params.course_id);
-  collection.splice(req.courseIndex, 1);
-  res.status(204).end();
-});
+app.use('/courses', courses);
 app.get("/error", (req, res) => {
   throw new Error("oh no !!!");
 });
