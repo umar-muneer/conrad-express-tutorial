d278ea887bb9a16cc1e464bce838d8658dbc9818
Umar Muneer <umarmuneer@gmail.com>
2021-01-11 11:43:21 +0500

    add global middleware for logging express requests

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bunyanLogger = require("express-bunyan-logger");
 const collection = [];
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
 const checkCourseExists = (req, res, next) => {
   const courseId = parseInt(req.params.course_id);
-  const courseIndex = collection.findIndex(course => course.id === courseId);
+  const courseIndex = collection.findIndex((course) => course.id === courseId);
   if (courseIndex === -1) {
     res.status(404).json({
-      message: 'requested resource was not found'
+      message: "requested resource was not found",
     });
     return;
   }
@@ -18,48 +18,58 @@ const checkCourseExists = (req, res, next) => {
   next();
 };
 const validateCoursePostRequest = (req, res, next) => {
-  const {id, name} = req.body;
-  console.log(parseInt(id));
+  const { id, name } = req.body;
   if (isNaN(parseInt(id))) {
     res.status(400).json({
-      message: 'the id of the course should be a number'
+      message: "the id of the course should be a number",
     });
     return;
   }
   if (!name) {
     res.status(400).json({
-      message: 'the name of the course is required'
+      message: "the name of the course is required",
     });
     return;
   }
   next();
 };
+app.use(bodyParser.json());
+app.use(bunyanLogger());
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).json("oh no, sth went wrong !!");
+    req.log.error(err);
+    return;
+  }
+});
 app.get("/health", (req, res) => res.json("OK"));
-
 app.post("/courses", validateCoursePostRequest, (req, res) => {
   const { id, name } = req.body;
   collection.push({ id, name });
   res.json({
     id,
-    name
+    name,
   }); // sending a json request with the collection array as the body and status 200
 });
 app.get("/courses", (req, res) => {
   res.json(collection);
 });
 app.put("/courses/:course_id", checkCourseExists, (req, res) => {
-  const {name} = req.body;
+  const { name } = req.body;
   const courseId = parseInt(req.params.course_id);
   const updatedCourse = {
     id: courseId,
-    name
+    name,
   };
   collection[req.courseIndex] = updatedCourse;
   res.status(200).json(collection[courseIndex]);
 });
-app.delete('/courses/:course_id', checkCourseExists, (req, res) => {
+app.delete("/courses/:course_id", checkCourseExists, (req, res) => {
   const courseId = parseInt(req.params.course_id);
   collection.splice(req.courseIndex, 1);
   res.status(204).end();
+});
+app.get("/error", (req, res) => {
+  throw new Error("oh no !!!");
 });
 app.listen(port, () => console.log(`listening on port ${port}`));
