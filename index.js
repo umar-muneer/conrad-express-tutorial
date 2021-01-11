a9de97d816b9e0acdd47d7834838a0c778b69323
Umar Muneer <umarmuneer@gmail.com>
2021-01-11 09:32:28 +0500

    add a post route with json body

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const collection = [];
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/health', (req, res) => res.json("OK"));
+app.use(bodyParser.json());
+app.get("/health", (req, res) => res.json("OK"));
 
+app.post("/courses", (req, res) => {
+  const { id, name } = req.body;
+  collection.push({ id, name });
+  res.json(collection); // sending a json request with the collection array as the body and status 200
+});
 app.listen(port, () => console.log(`listening on port ${port}`));
